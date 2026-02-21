ce64682ccc588fde4fa850ebf8059412c337bce3
Abhinav Kumar <Dynamic2106@gmail.com>
2026-02-22 01:55:00 +0530

    #Added Handler

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //If there is no response form cmd then this interface will work
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+
+    }
+
+    public interface ICommandHandler<in TCommand, TResposne> : IRequestHandler<TCommand, TResposne>
+        where TCommand : ICommand<TResposne>
+        where TResposne : notnull
+    {
+    }
+}
