e12619b1d9ff1a842ee46fcddfcf59d0c6bf2a0c
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-02-23 00:58:06 +0530

    Added Endpoint call logic (#6)

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
 
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
@@ -10,7 +8,22 @@ namespace Catalog.API.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/product", async (CreateProductRequest request, ISender sender) =>
+            {
+                //MediatR need command obj in order to trigger CH.
+                var command = request.Adapt<CreateProductCommand>();  //Created request cmd mapped using Mapster
+
+                var result = await sender.Send(command); // start MediatR andTrigger Handler
+
+                var response = result.Adapt<CreateProductResponse>(); //Created Response and mapped using Mapster
+
+                return Results.Created($"/products/{response.id}", response);
+            })
+                .WithName("CreateProduct") //Name of the Http Post Method
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created) //Response
+                .ProducesProblem(StatusCodes.Status400BadRequest) //Error
+                .WithSummary("Created Product") // Sumary of the Endpoint
+                .WithDescription("Create product"); //Description of the endpoint
         }
     }
 }
