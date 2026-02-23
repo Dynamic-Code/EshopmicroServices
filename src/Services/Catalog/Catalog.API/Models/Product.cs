69521df2472d07f02acd3b4dfdcef752016fb32e
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-02-24 00:47:52 +0530

    #ep 54 (#8)
    * Added Carter in Catelog and removed from Building blocs
    
    * BUG FIX

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = default!;
         public List<string> Category { get; set; } = new();
         public string Description { get; set; } = default!;
