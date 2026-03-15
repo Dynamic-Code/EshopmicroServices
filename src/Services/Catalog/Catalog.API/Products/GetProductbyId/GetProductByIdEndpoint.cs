0cae4915202e4b21ffdcb021ee711bf9c972dc78
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-16 00:55:56 +0530

    implemented endpoint (#25)

--- src/Services/Catalog/Catalog.API/Products/GetProductbyId/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductbyId/GetProductByIdEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.GetProductbyId
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductbyId
 {
-    public class GetProductByIdEndpoint
+    //public record GetProductbyIdRequest();
+    public record getProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductResponse>();
+                return response;
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product by Id")
+            .WithDescription("Get Single Product");
+        }
     }
 }
