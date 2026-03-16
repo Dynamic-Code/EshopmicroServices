2397bfb784bcf9eab7d9d74c55c3bfae1fe509ab
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-16 22:31:58 +0530

    #EP-75 Bug Fix for GetProductByCategory (#29)

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -2,14 +2,15 @@
 {
     public record GetProductByCategoryQuery(string Category) : IQuery<GetproductByCategoryResult>;
     public record GetproductByCategoryResult(IEnumerable<Product> Products);
-    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger logger) : IQueryHandler<GetProductByCategoryQuery, GetproductByCategoryResult>
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) 
+        : IQueryHandler<GetProductByCategoryQuery, GetproductByCategoryResult>
     {
         public async Task<GetproductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
         {
             logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
 
-            var result = await session.Query<Product>().
-                Where(p => p.Category.Contains(query.Category)).ToListAsync();
+            var result = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category)).ToListAsync(cancellationToken);
             return new GetproductByCategoryResult(result);
         }
     }
