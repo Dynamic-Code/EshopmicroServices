e0e7763c1f48439c2ca5a08d77bb781d918565bb
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-16 23:28:29 +0530

    #EP-79-> Added DeleteProduct Endpoint (#33)

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,6 +1,23 @@
 ﻿namespace Catalog.API.Products.DeleteProduct
 {
-    public class DeleteProductEndpoint
+    //public record DeteleProductRequest(Guid id);
+    public record DeteleProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+                var response = result.Adapt<DeteleProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeteleProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
     }
 }
