99954f39cdfbe7a14af55e27e758ba2aa593bfa5
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-19 23:29:12 +0530

    #EP-86 :- Removed old validation (#38)
    Refactored program.cs

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -18,17 +18,12 @@ namespace Catalog.API.Products.CreateProduct
         }
     }
 
-    internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Handler check against our validation rules and if there is any error, it will throw errors exception
-            var result = await validator.ValidateAsync(command, cancellationToken);
-            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-            if (errors.Any())
-            {
-                throw new ValidationException(errors.FirstOrDefault());
-            }
+
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", command);
 
             //Create Product Entity
             var product = new Product
