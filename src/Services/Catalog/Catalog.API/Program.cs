e8b508f75b128898d7a4a9626125a76666ad95a7
Abhinav Kumar <Dynamic2106@gmail.com>
2026-03-20 00:30:49 +0530

    #EP-88 :- Added a global exception handeling to structure the error

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Behaviours;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +25,32 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+//Added  Global asp.net exception handler to catch unhandled exceptions and return a standardized error response
+app.UseExceptionHandler(exceptionhandlerApp =>
+{
+    exceptionhandlerApp.Run(async eontext =>
+    {
+        var exception = eontext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = eontext.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        eontext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        eontext.Response.ContentType = "application/problem+json";
+
+        await eontext.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
