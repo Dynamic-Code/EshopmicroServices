159cc4e08278534ce5cbfdefe7de30615fc4f554
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-20 00:57:06 +0530

    #EP-89 :- Added generic exception in Building Block sln and used in our Catelog proj (#42)

--- src/Services/Catalog/Catalog.API/Products/GetProductbyId/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductbyId/GetProductByIdHandler.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Catalog.API.Products.GetProductbyId
 {
-    public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 
     public record GetProductByIdResult(Product Product);
     internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQuery> logger)
@@ -11,10 +11,10 @@ namespace Catalog.API.Products.GetProductbyId
         {
             logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
 
-            var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
             if (product == null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(query.Id);
             }
 
             return new GetProductByIdResult(product);
