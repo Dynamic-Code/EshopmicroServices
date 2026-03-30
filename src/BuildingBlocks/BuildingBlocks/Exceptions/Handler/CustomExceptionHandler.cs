62996c15c624988633b96734971e900e311738fc
Abhinav <65800048+Dynamic-Code@users.noreply.github.com>
2026-03-30 23:57:19 +0530

    #EP-91 Added custom validation (#44)

--- src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 
 namespace BuildingBlocks.Exceptions.Handler
 {
-    public class CustomExceptionHandler(ILogger logger)
+    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
         : IExceptionHandler
     {
         public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
